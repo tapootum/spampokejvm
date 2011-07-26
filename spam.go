855c47853eb90d406b14b8f345872042bb153590
tapootum <tapootumchannel@gmail.com>
2011-07-27 01:59:59 +0700

    go ruotine  Gwean1 and channel grean

--- spam.go
+++ spam.go
@@ -1,30 +1,49 @@
 package main
 
 import (
-	
+	"time"
+	"fmt"
+	"runtime"
 	"smtp"
 	"log"
 	)
+var c = make (chan int)
+func sendmail (){
 
-func main() {
-
-	auth := smtp.PlainAuth(
+		c <- 1
+	
+		auth := smtp.PlainAuth(
 			"",
-			"gwean@grean.com",
-			"password",
+			"tapootumfb@gmail.com",
+			"kawaoisoki15124",
 			"smtp.gmail.com",
 		)
-	//for i:=1; i<=5 ; i++ {
 		err := smtp.SendMail(
-				"smtp.gmail.com:587",
-				auth,
-				"tapootumfb@gmail.com",
-				[]string{"tapootumfb@gmail.com"},
-				[]byte("Gwean send spam mail"),
+			"smtp.gmail.com:587",
+			auth,
+			"tapootumfb@gmail.com",
+			[]string{"tapootumfb@gmail.com"},
+			[]byte("Gwean send spam mailgo "),
 			)
 		if err != nil {
 			log.Fatal(err)
 			}
-	//	}
+
+	
+
+}
+
+func main() {
+	n := 5
+	runtime.GOMAXPROCS(n)
+	
+	for i:=1; i<=5 ; i++ {
+	go  sendmail()		
+        time.Sleep(600000)	
+	<- c
+	fmt.Printf("Email %d\n",i)	
+	
+	}
+
 
 }

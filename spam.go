84b46578b3fde78245f2c7cb40a597b7417d9333
tapootum <tapootumchannel@gmail.com>
2011-08-17 17:11:26 +0700

    Spam Text File and Subject File

--- spam.go
+++ spam.go
@@ -8,17 +8,20 @@ import (
 	"strconv"
 	"rand"
 	"io/ioutil"
+	"runtime"
 	)
 var wait = make (chan int)
-func sendmail (file string){
-
-		st , _ := ioutil.ReadFile(file)
-		msg := string(st)
-		
+func sendmail (file string, dir string){
+		da := "subject/" + dir
+		ms , _ := ioutil.ReadFile(file)
+		sub , _ := ioutil.ReadFile(da)
+		subj := string(sub)
+		mss := string(ms)
+		msg := "Subject:" + subj + "\n" + mss
 		auth := smtp.PlainAuth(
 			"",
 			"spam@tapootum.com",
-			"XXXXX",
+			"kawaoisoki",
 			"tapootum.com",
 		)
 		err := smtp.SendMail(
@@ -36,7 +39,7 @@ func sendmail (file string){
  wait <- 1
 }
 func main() { 
-
+	runtime.GOMAXPROCS(4)
 	if len(os.Args) == 1 {
 		fmt.Println("Don't send mail")
 	}else {
@@ -48,7 +51,11 @@ func main() {
 		num := rand.Int63n(20)
 		ff := strconv.Itoa64(num)
 		file := ff + ".txt"
-		go sendmail(file)
+
+		num2 := rand.Int63n(20)
+		aa := strconv.Itoa64(num2)
+		dir := aa + ".txt"
+		go sendmail(file,dir)
 		}
 	for i:=1 ; int64(i)<=n ; i++{
 		<- wait

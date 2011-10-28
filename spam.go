0694c6e49d41b9533241f66da510f5da0dc6cf44
tapootum <tapootumchannel@gmail.com>
2011-10-28 15:06:13 +0700

    Complete spam

--- spam.go
+++ spam.go
@@ -14,14 +14,33 @@ import (
 
 var wait = make (chan int)
 var count int
-
+var sumTime float64
 // AUTHEN 
 var auth = smtp.PlainAuth(
                         "",
-                        "anchalee@bangkokranch.co.th",
-                        "11111",
+                        "",
+                        "",
                         "mail2.bangkokranch.co.th",  // smtp protocal //No port
                 )
+
+const ( 
+  dateStamp="2006-01-02-15:04:05" 
+  dateLayout="20060102150405" 
+) 
+
+func getTime() { 
+ // var ds string 
+  //t:=time.Seconds() 
+  lt:=time.LocalTime() 
+  ts:=lt.Format(dateStamp) 
+  //ds="2011-01-28-16:15:00" 
+  //tp,_:=time.Parse(dateStamp, ds) 
+  //println("lt: ",lt) 
+  //print("Unix Epoch: ",t,"\n") 
+  print("Start Time: ",ts,"\n") 
+}
+
+
 
 func sendmail (){
 	//	random file	
@@ -45,7 +64,7 @@ func sendmail (){
 		err := smtp.SendMail(
 			"mail2.bangkokranch.co.th:25",  
 			auth,
-			"anchalee@mail2bangkokranch.co.th",   // from 
+			"tapootumchannel@gmail.com",   // from 
 			[]string{"anchalee@bangkokranch.co.th"},  //to
 			[]byte(msg),
 			)
@@ -53,67 +72,77 @@ func sendmail (){
 			log.Fatal(err)
 			}
 		count++
-		fmt.Print(count)
-		fmt.Println("   Send mail")
+//		fmt.Print(count)
+//		fmt.Println("   Send mail")
 	// channel
-	wait <- 1
+//	wait <- 1
 
 }
 
-func send(n int64){
+func send(n int64,x int){
+	timee1 , time1 , _ := os.Time()
 		for i:=1 ; int64(i)<=n ; i++ {
-			go sendmail()
+			 sendmail()
 		}
 
-			fmt.Printf("|||||||||||||||||||||| %d\n",n)
+//			fmt.Printf("|||||||||||||||||||||| %d\n",n)
 
-		for j:=1 ; int64(j)<=n ; j++{
-			<- wait
-		}
+//		for j:=1 ; j<=10 ; j++{
+//			<- wait
+//		}
 
+	timee2 , time2 , _ := os.Time()
+	time := time2 - time1
+	timee := timee2 - timee1
+	times := float64(1e9*timee+time)*0.000000001
+	fmt.Printf("Thread %d time = %.2f s\n",x,times)
+
+	sumTime = sumTime + float64(times)
+
+	wait <- 1
 }
 
 func main() {
-	go send(1)
-//		<- wait
-	times1 , _ , _ := os.Time()
+	print("\n\n")
+	getTime()
+	print("\n")
+	times1 , timen1 , _ := os.Time()
 	runtime.GOMAXPROCS(4)
 	if len(os.Args) == 1 {
 		fmt.Println("Don't send mail")
 	}else {
 
 	s := os.Args[1]
-	n , _ := strconv.Atoi64(s)
+	t , _ := strconv.Atoi64(s)
+	y := os.Args[2]
+	m , _ := strconv.Atoi64(y)
 
-	var tt1 int64
-	tt1 = 0
-	v := 0
-//		go sendmail()
-//		<- wait
-		for {
-			tt2:=time.Seconds()
-				if tt1 < tt2 {
-					go send(n)
-					v++
-					//wait <- 1
-				}
-				if v == 5{
-					break
-					fmt.Println("tttttttttttttttttttttttttttttttttttttttttt")
-				}
-			tt1=tt2
+
+		for i:=1 ; int64(i)<=t ; i++{
+			go send(m,i)
 		}
-	}
 
-
-		for j:=1 ; int64(j)<=5 ; j++{
+		for i:=1 ; int64(i)<=t ; i++{
 			<- wait
 		}
 
-	times2 , _ , _ := os.Time()
+
+
+
+	times2 , timen2 , _ := os.Time()
 	tt := times2-times1
-		fmt.Printf("Send spam mail = ")
-		fmt.Println(count)
-		fmt.Printf(".............%d s...........\n",tt)
+	nn := timen2-timen1
+	time := float64(1e9*tt+nn)*0.000000001
+//	mm := 60
+		avgTime := sumTime / float64(t)
+		fmt.Println("\n==========================================================")
+		fmt.Printf("%d Thread\n%d Mail\n",t,m)
+		fmt.Printf("Send spam %d mail\n",count)
+                fmt.Printf("All time send %.2f s\n",time)
+		fmt.Printf("Average thread time = %.2f s\n",avgTime)
+		fmt.Printf("Average = %.2f / mail\n",avgTime/float64(t*m))
+		fmt.Println("===========================================================")
+
+	}
 
 }
